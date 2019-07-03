0a72112d86628048d76a409a9629b002bb5ba079
Vitaly Ostrosablin <tmp6154@yandex.ru>
2019-07-03 09:54:41 +0300

    Fixup setup.py
    Signed-off-by: Vitaly Ostrosablin <tmp6154@yandex.ru>

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/tmp6154/pyttygif",
-    packages=[],
+    packages=setuptools.find_packages(),
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Console",
