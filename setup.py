a602d21fc22f59dd6c6518d8488e2cd20545d909
Vitaly Ostrosablin <tmp6154@yandex.ru>
2019-07-03 09:30:20 +0300

    Add setup.py
    Signed-off-by: Vitaly Ostrosablin <tmp6154@yandex.ru>

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyttygif",
+    version="0.1.0",
+    author="Vitaly Ostrosablin",
+    author_email="tmp6154@yandex.ru",
+    description="ttyrec to GIF converter",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tmp6154/pyttygif",
+    packages=[],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX",
+        "Topic :: Multimedia :: Graphics :: Capture :: Screen Capture",
+        "Topic :: Terminals :: Terminal Emulators/X Terminals",
+    ],
+) 
